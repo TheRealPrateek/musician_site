8855c0573e947f1f2b0be24df6a1f9911ca963dd
Prateek Poddar <poddarp312@gmail.com>
2023-09-15 13:50:59 -0400

    changed site to single page format, multi-page on separate branch

---
+++ public/js/scripts.js
@@ -0,0 +1,23 @@
+function about() {
+    window.scrollTo(0, document.getElementById("about").offsetTop-115);
+} 
+
+function music() {
+    window.scrollTo(0, document.getElementById("music").offsetTop-115);
+}
+
+function press() {
+    window.scrollTo(0, document.getElementById("press").offsetTop-115);
+}
+
+function video() {
+    window.scrollTo(0, document.getElementById("video").offsetTop-115);
+}
+
+function contact() {
+    window.scrollTo(0, document.getElementById("contact").offsetTop-115);
+}
+
+function subscribe() {
+    window.scrollTo(0, document.getElementById("subscribe").offsetTop-115);
+}
