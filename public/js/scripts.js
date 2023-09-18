bdc0be7ad5cfe48b5c0f0cc89c5b3bff0aa2f954
Prateek Poddar <poddarp312@gmail.com>
2023-09-18 10:33:24 -0400

    more changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,24 @@
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+const menuIcon = document.querySelector(".hburgmenu .fa-bars");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    menuIcon.style.display = "none";
+  }
+};
+
+window.onscroll = function removeMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    menuIcon.style.display = "block";
+  }
+};
+
 function about() {
     window.scrollTo(0, document.getElementById("about").offsetTop-115);
 } 
