3fc97ad596f5c01e08475322a26e77125bd11df9
Prateek Poddar <poddarp312@gmail.com>
2024-03-20 13:43:43 -0400

    changed navigation to jump links

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,30 +19,30 @@ window.onscroll = function removeMenu() {
   }
 };
 
-function about() {
-    window.scrollTo(0, document.getElementById("about").offsetTop-115);
-} 
+// function about() {
+//     window.scrollTo(0, document.getElementById("about").offsetTop-115);
+// } 
 
-function music() {
-    window.scrollTo(0, document.getElementById("music").offsetTop-115);
-}
+// function music() {
+//     window.scrollTo(0, document.getElementById("music").offsetTop-115);
+// }
 
-function press() {
-    window.scrollTo(0, document.getElementById("press").offsetTop-115);
-}
+// function press() {
+//     window.scrollTo(0, document.getElementById("press").offsetTop-115);
+// }
 
-function video() {
-    window.scrollTo(0, document.getElementById("video").offsetTop-115);
-}
+// function video() {
+//     window.scrollTo(0, document.getElementById("video").offsetTop-115);
+// }
 
-function shows() {
-    window.scrollTo(0, document.getElementById("shows").offsetTop-115);
-}
+// function shows() {
+//     window.scrollTo(0, document.getElementById("shows").offsetTop-115);
+// }
 
-function contact() {
-    window.scrollTo(0, document.getElementById("contact").offsetTop-115);
-}
+// function contact() {
+//     window.scrollTo(0, document.getElementById("contact").offsetTop-115);
+// }
 
-function subscribe() {
-    window.scrollTo(0, document.getElementById("subscribe").offsetTop-115);
-}
+// function subscribe() {
+//     window.scrollTo(0, document.getElementById("subscribe").offsetTop-115);
+// }
