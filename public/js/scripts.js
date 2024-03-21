e6de89886b6a2880ada7773bcd9310c36083b293
Prateek Poddar <prateek@Prateeks-MacBook-Pro.local>
2024-03-21 15:39:54 -0400

    added spotify api to render spotify iframe

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,17 @@ const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu-item");
 const menuIcon = document.querySelector(".hburgmenu .fa-bars");
 
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  const element = document.getElementById('embed-iframe');
+  const options = {
+      width: '100%',
+      height: '500',
+      uri: 'spotify:album:0ldTZ7gtbDR7x2Nxhm4E8z'
+    };
+  const callback = (EmbedController) => {};
+  IFrameAPI.createController(element, options, callback);
+};
+
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
