697132608cef39b968c65dde315982cf8b38ddee
Prateek Poddar <prateek@Prateeks-MacBook-Pro.local>
2024-03-22 11:55:30 -0400

    experimenting with splitting up js files to help with performance

---
+++ public/js/spotifyScripts.js
@@ -0,0 +1,10 @@
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        width: '100%',
+        height: '500',
+        uri: 'spotify:album:0ldTZ7gtbDR7x2Nxhm4E8z'
+    };
+    const callback = (EmbedController) => {};
+    IFrameAPI.createController(element, options, callback);
+};
