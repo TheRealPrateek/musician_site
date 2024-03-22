ae1807945267c295994ddef2d821ca4a4a995dee
Prateek Poddar <prateek@Prateeks-MacBook-Pro.local>
2024-03-22 12:54:13 -0400

    implementing lazysizes plugin

--- public/js/spotifyScripts.js
+++ public/js/spotifyScripts.js
@@ -3,6 +3,7 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
     const options = {
         width: '100%',
         height: '500',
+        class: 'lazyload',
         uri: 'spotify:album:0ldTZ7gtbDR7x2Nxhm4E8z'
     };
     const callback = (EmbedController) => {};
